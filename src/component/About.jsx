714bad6da224551c94bfa7351fab400452753b82
mohaanpoi7 <mohaanpoi98@gmail.com>
2024-07-29 07:19:57 -0700

    update

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className="min-h-screen bg-gray-800 flex items-center justify-center p-5">
       <div className="bg-gray-900 text-white rounded-lg shadow-lg p-6 max-w-md w-full">
         <h2 className="text-2xl font-bold text-center mb-6">About</h2>
-        <p className="text-center">This is the About Page</p>
+        <p className="text-center">welcome all </p>
       </div>
     </div>
   );
